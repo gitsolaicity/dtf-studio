bb28aa5a203806a6cf0fde7ecb700704eda220d9
gitsolaicity <76422002+gitsolaicity@users.noreply.github.com>
2025-07-13 12:43:44 +0300

    added_speedInsights_vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Providers } from "./providers";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "BlackLight365",
@@ -11,7 +12,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ru" suppressHydrationWarning>
       <body className="bg-white text-gray-900 dark:bg-black dark:text-white transition-colors duration-300">
-        <Providers>{children}<Analytics /></Providers>
+        <Providers>
+          {children}
+          <Analytics />
+          <SpeedInsights />
+          </Providers>
       </body>
     </html>
   );
