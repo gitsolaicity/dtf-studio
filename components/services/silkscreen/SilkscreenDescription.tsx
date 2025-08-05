d82daf67d4c992d4939de2c31b7e359cfdb39b24
gitsolaicity <76422002+gitsolaicity@users.noreply.github.com>
2025-08-05 15:27:02 +0300

    2

--- components/services/silkscreen/SilkscreenDescription.tsx
+++ components/services/silkscreen/SilkscreenDescription.tsx
@@ -1,6 +1,6 @@
 export default function SilkscreenDescription() {
   return (
-    <section id="description" className="scroll-mt-32">
+    <section id="description" className="scroll-mt-42">
       <h2 className="inline-block text-3xl md:text-3xl font-bold mb-6 px-4 py-2 rounded-lg border border-white/10 bg-gradient-to-r from-white/5 to-white/0 text-[var(--accent)] shadow-sm backdrop-blur-sm">
   Шовкографія
 </h2>
